122f7bbf0bce410e4ffb18a79f9de52c3aa386b6
TheButlerDidIt <labutler@gmail.com>
2017-03-16 14:24:12 -0400

    exercise #4

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,24 @@
+
+function DnaTranscriber() {
+
+	this.toRna = function(dna){
+		var rna = '';
+
+		for (var i = 0; i < dna.length; i++) {
+			if (dna[i] === 'G') {
+				rna += 'C';
+			} else if (dna[i] === 'C') {
+				rna += 'G';
+			} else if (dna[i] === 'T') {
+				rna += 'A';
+			} else if (dna[i] === 'A') {
+				rna += 'U';
+			} else {
+				throw new Error('Invalid input');
+			}
+		}
+		return rna;
+	}
+};
+
+module.exports = DnaTranscriber;
