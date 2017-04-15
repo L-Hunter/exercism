2a2ebc6ab91dbc6d215980a47737aeff49448be5
TheButlerDidIt <labutler@gmail.com>
2017-04-15 08:48:16 -0400

    refactors exercise #12

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,59 +1,53 @@
 function FoodChain(){
-	this.verse = function(start){
-		var lyrics = "";
-		
-		switch (start) {
-  			case 1:
-    			lyrics = "I know an old lady who swallowed a fly.\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n";
 
-    			break;
-    		case 2:
-    			lyrics = 'I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\n' + 'She swallowed the spider to catch the fly.\n' + 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n';
-    			break;
-    		case 3:
-    			lyrics = 'I know an old lady who swallowed a bird.\n' + 'How absurd to swallow a bird!\n' + 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n' + 'She swallowed the spider to catch the fly.\n' + 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n';
-				break;
-			case 4:
-				lyrics = 'I know an old lady who swallowed a cat.\n' + 'Imagine that, to swallow a cat!\n' + 'She swallowed the cat to catch the bird.\n' + 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n' + 'She swallowed the spider to catch the fly.\n' + 'I don\'t know why she swallowed the fly. ' + 'Perhaps she\'ll die.\n';
-				break;
-			case 5:
-				lyrics = 'I know an old lady who swallowed a dog.\n' + 'What a hog, to swallow a dog!\n' + 'She swallowed the dog to catch the cat.\n' + 'She swallowed the cat to catch the bird.\n' + 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n' + 'She swallowed the spider to catch the fly.\n' + 'I don\'t know why she swallowed the fly. ' + 'Perhaps she\'ll die.\n';
-				break;
-			case 6: 
-				lyrics = 'I know an old lady who swallowed a goat.\n' + 'Just opened her throat and swallowed a goat!\n' + 'She swallowed the goat to catch the dog.\n' + 'She swallowed the dog to catch the cat.\n' + 'She swallowed the cat to catch the bird.\n' + 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n' + 'She swallowed the spider to catch the fly.\n' + 'I don\'t know why she swallowed the fly. ' + 'Perhaps she\'ll die.\n';
-				break;
-			case 7:
-				lyrics = 'I know an old lady who swallowed a cow.\n' + 'I don\'t know how she swallowed a cow!\n' +'She swallowed the cow to catch the goat.\n' + 'She swallowed the goat to catch the dog.\n' + 'She swallowed the dog to catch the cat.\n' + 'She swallowed the cat to catch the bird.\n' + 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n' + 'She swallowed the spider to catch the fly.\n' + 'I don\'t know why she swallowed the fly. ' + 'Perhaps she\'ll die.\n';
-				break;
-    		case 8:
-    			lyrics = 'I know an old lady who swallowed a horse.\n' + 'She\'s dead, of course!\n';
-    			break;
-    	}
-    	return lyrics;
+	var food = {
+		"IK": "I know an old lady who swallowed a ",
+		"fly": 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n',
+		"spider": 'She swallowed the spider to catch the fly.\n',
+		"bird": 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n',
+		"cat": 'She swallowed the cat to catch the bird.\n',
+		"dog": 'She swallowed the dog to catch the cat.\n',
+		"goat": 'She swallowed the goat to catch the dog.\n',
+	}
+
+	var song = {
+		1: food["IK"] + 'fly.\n' + food["fly"],
+		2: food["IK"] + 'spider.\nIt wriggled and jiggled and tickled inside her.\n' + food["spider"] + food["fly"],
+    	3: food["IK"] + 'bird.\nHow absurd to swallow a bird!\n' + food["bird"] + food["spider"] + food["fly"],
+		4: food["IK"] + 'cat.\nImagine that, to swallow a cat!\n' + food["cat"] + food["bird"] + food["spider"] + food["fly"],
+		5: food["IK"] + 'dog.\nWhat a hog, to swallow a dog!\n' + food["dog"] + food["cat"] + food["bird"] + food["spider"] + food["fly"],
+		6: food["IK"] + 'goat.\nJust opened her throat and swallowed a goat!\n' + food["goat"] + food["dog"] + food["cat"] + food["bird"] + food["spider"] + food["fly"],
+		7: food["IK"] + 'cow.\nI don\'t know how she swallowed a cow!\nShe swallowed the cow to catch the goat.\n' + food["goat"] + food["dog"] + food["cat"] + food["bird"] + food["spider"] + food["fly"],
+		8: food["IK"] + 'horse.\nShe\'s dead, of course!\n',
+	}
+	
+	this.verse = function(start){
+    	return song[start];
     }
 
     this.verses = function(start, end){
 		lyrics = "";
 
 		if (end >= 1) {
-			lyrics += "I know an old lady who swallowed a fly.\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n\n";
+			lyrics += song[1] + '\n';
 		} if (end >= 2) {
-			lyrics += 'I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\n' + 'She swallowed the spider to catch the fly.\n' + 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n\n';
+			lyrics += song[2] + '\n';
 		} if (end >= 3) {
-			lyrics += 'I know an old lady who swallowed a bird.\n' + 'How absurd to swallow a bird!\n' + 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n' + 'She swallowed the spider to catch the fly.\n' + 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n\n';
+			lyrics += song[3] + '\n';
 		} if (end >= 4) {
-			lyrics += 'I know an old lady who swallowed a cat.\n' + 'Imagine that, to swallow a cat!\n' + 'She swallowed the cat to catch the bird.\n' + 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n' + 'She swallowed the spider to catch the fly.\n' + 'I don\'t know why she swallowed the fly. ' + 'Perhaps she\'ll die.\n\n';
+			lyrics += song[4] + '\n';
 		} if (end >= 5) {
-			lyrics += 'I know an old lady who swallowed a dog.\n' + 'What a hog, to swallow a dog!\n' + 'She swallowed the dog to catch the cat.\n' + 'She swallowed the cat to catch the bird.\n' + 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n' + 'She swallowed the spider to catch the fly.\n' + 'I don\'t know why she swallowed the fly. ' + 'Perhaps she\'ll die.\n\n';
+			lyrics += song[5] + '\n';
 		} if (end >= 6) {
-			lyrics += 'I know an old lady who swallowed a goat.\n' + 'Just opened her throat and swallowed a goat!\n' + 'She swallowed the goat to catch the dog.\n' + 'She swallowed the dog to catch the cat.\n' + 'She swallowed the cat to catch the bird.\n' + 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n' + 'She swallowed the spider to catch the fly.\n' + 'I don\'t know why she swallowed the fly. ' + 'Perhaps she\'ll die.\n\n';
+			lyrics += song[6] + '\n';
 		} if (end >= 7) {
-			lyrics += 'I know an old lady who swallowed a cow.\n' + 'I don\'t know how she swallowed a cow!\n' +'She swallowed the cow to catch the goat.\n' + 'She swallowed the goat to catch the dog.\n' + 'She swallowed the dog to catch the cat.\n' + 'She swallowed the cat to catch the bird.\n' + 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n' + 'She swallowed the spider to catch the fly.\n' + 'I don\'t know why she swallowed the fly. ' + 'Perhaps she\'ll die.\n\n';
+			lyrics += song[7] + '\n';
 		} if (end >= 8) {
-			lyrics += 'I know an old lady who swallowed a horse.\n' + 'She\'s dead, of course!\n\n';
+			lyrics += song[8] + '\n';
 		}
 		return lyrics;
 	}
     
 }
+
 module.exports = FoodChain;
